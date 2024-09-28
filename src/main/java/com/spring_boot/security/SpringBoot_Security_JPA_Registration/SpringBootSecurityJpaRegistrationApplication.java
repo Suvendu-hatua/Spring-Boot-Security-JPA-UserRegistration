5de9b248a39048daeb1ad73e4f13b046113bb7c7
Suvendu Hatua <suvenduhatua01@gmail.com>
2024-09-28 22:40:46 +0530

    Adding all the initial files and folder for the project

---
+++ src/main/java/com/spring_boot/security/SpringBoot_Security_JPA_Registration/SpringBootSecurityJpaRegistrationApplication.java
@@ -0,0 +1,13 @@
+package com.spring_boot.security.SpringBoot_Security_JPA_Registration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSecurityJpaRegistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSecurityJpaRegistrationApplication.class, args);
+	}
+
+}
