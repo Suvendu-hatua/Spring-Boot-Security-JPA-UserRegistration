5a37070f4a615583092b76176dc30a5b7fd4fb1e
Suvendu Hatua <suvenduhatua01@gmail.com>
2024-09-29 12:30:47 +0530

    Adding DemoController for accessing homepage and other endpoints.

---
+++ src/main/java/com/spring_boot/security/SpringBoot_Security_JPA_Registration/controllers/DemoController.java
@@ -0,0 +1,12 @@
+package com.spring_boot.security.SpringBoot_Security_JPA_Registration.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DemoController {
+    @GetMapping("/")
+    public String homePage(){
+        return "home";
+    }
+}
