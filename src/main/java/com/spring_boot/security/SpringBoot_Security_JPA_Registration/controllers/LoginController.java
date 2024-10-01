551fbf4f2f97f47b5c1bbb44d1d9d89f37fd6772
Suvendu Hatua <suvenduhatua01@gmail.com>
2024-10-01 20:26:37 +0530

    Adding LoginController

---
+++ src/main/java/com/spring_boot/security/SpringBoot_Security_JPA_Registration/controllers/LoginController.java
@@ -0,0 +1,12 @@
+package com.spring_boot.security.SpringBoot_Security_JPA_Registration.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+    @GetMapping("/show-login")
+    public String showLogin(){
+        return "custom-signing";
+    }
+}
