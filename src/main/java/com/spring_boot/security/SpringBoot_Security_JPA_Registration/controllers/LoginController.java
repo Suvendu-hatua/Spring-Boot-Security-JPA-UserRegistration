b4b5c1ac533163fef18ed3297a38d34bc087fb54
Suevndu Hatua <suvenduhatua01@gmail.com>
2025-02-05 22:11:09 +0530

    Adding '/account' endpoint in LoginController

--- src/main/java/com/spring_boot/security/SpringBoot_Security_JPA_Registration/controllers/LoginController.java
+++ src/main/java/com/spring_boot/security/SpringBoot_Security_JPA_Registration/controllers/LoginController.java
@@ -1,14 +1,29 @@
 package com.spring_boot.security.SpringBoot_Security_JPA_Registration.controllers;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@Slf4j
 public class LoginController {
     @GetMapping("/show-login")
     public String showLogin(){
         return "custom-signing";
     }
+
+    @GetMapping("/account")
+    public String login(){
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        //Extracting username
+        String username = auth.getName();
+        log.info(username + " " + auth.getAuthorities());
+
+        return "registration/complete-profile";
+    }
+
 
     @GetMapping("access-denied")
     public String accessDeniedPage(){
