1877f40294d1d17273f3617629fcb0d2167513ec
Suevndu Hatua <suvenduhatua01@gmail.com>
2025-02-08 12:55:47 +0530

    Adding ApplicantController and implemented submitJobApplication method

---
+++ src/main/java/com/spring_boot/security/SpringBoot_Security_JPA_Registration/controllers/ApplicantController.java
@@ -0,0 +1,35 @@
+package com.spring_boot.security.SpringBoot_Security_JPA_Registration.controllers;
+
+import com.spring_boot.security.SpringBoot_Security_JPA_Registration.service.ApplicantService;
+import com.spring_boot.security.SpringBoot_Security_JPA_Registration.user.ApplicantWebUser;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+@RequiredArgsConstructor
+@Slf4j
+public class ApplicantController {
+
+    private final ApplicantService applicantService;
+
+    @PostMapping("/submit-job-application")
+    public String submitJobApplication(@Valid  @ModelAttribute("applicant") ApplicantWebUser applicantWebUser, BindingResult bindingResult, Model model) {
+
+        if (bindingResult.hasErrors()) {
+            log.info(bindingResult.getAllErrors().toString());
+           log.error("Error in submit-job-application");
+            return "teacher/appointment";
+        }
+        //success
+        applicantService.submitApplicant(applicantWebUser);
+        model.addAttribute("applicant", new ApplicantWebUser());
+        model.addAttribute("status", true);
+        return "teacher/appointment";
+    }
+}
