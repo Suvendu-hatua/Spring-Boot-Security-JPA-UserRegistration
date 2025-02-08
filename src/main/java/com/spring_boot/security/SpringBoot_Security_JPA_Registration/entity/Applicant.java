3d08191084ca053fc903076d2c199a265a195ea6
Suevndu Hatua <suvenduhatua01@gmail.com>
2025-02-08 12:55:13 +0530

    Adding Applicant entity for teacher job application

---
+++ src/main/java/com/spring_boot/security/SpringBoot_Security_JPA_Registration/entity/Applicant.java
@@ -0,0 +1,31 @@
+package com.spring_boot.security.SpringBoot_Security_JPA_Registration.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDate;
+
+@Setter
+@Getter
+@ToString
+@Entity
+@Table(name = "applicant")
+public class Applicant {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phone;
+    private String address;
+    private String qualification;
+    private String appliedPosition;
+    private LocalDate appliedDate;
+    private String gender;
+    private String resume;
+    private String message;
+    private String status;
+}
